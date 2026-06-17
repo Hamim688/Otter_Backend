4751bb628b62f46f498e7bc9de62e5de5dd4ccf6
RafaEnricoo <muhammadrafaenrico@gmail.com>
2026-06-17 21:38:19 +0700

    feat: seed default sensor log on database setup

--- seeder/seeder.go
+++ seeder/seeder.go
@@ -21,9 +21,29 @@ func Seed(db *gorm.DB) {
 			AvatarURL:   "",
 		}
 		if err := db.Create(&dummyUser).Error; err == nil {
-			log.Println("[DATABASE SEED] Berhasil membuat akun dummy: admin / admin123")
+			log.Println("[DATABASE SEED] Berhasil membuat akun dummy: admin / 1234")
 		} else {
 			log.Println("[DATABASE SEED ERROR] Gagal membuat akun dummy:", err)
+		}
+	}
+
+	// === SEED DATA SENSOR AWAL (Mencegah Warning Record Not Found di GORM) ===
+	var sensorCount int64
+	db.Model(&models.SensorLog{}).Count(&sensorCount)
+	if sensorCount == 0 {
+		defaultSensor := models.SensorLog{
+			CahayaAtap:      80,
+			DapurSuhu:       28.0,
+			DapurKelembapan: 60.0,
+			DapurFlame:      0,
+			KamarSuhu:       27.0,
+			KamarKelembapan: 55.0,
+			TamuGerak:       false,
+		}
+		if err := db.Create(&defaultSensor).Error; err == nil {
+			log.Println("[DATABASE SEED] Berhasil membuat log sensor awal default.")
+		} else {
+			log.Println("[DATABASE SEED ERROR] Gagal membuat log sensor awal:", err)
 		}
 	}
 }
